fb5bb09af232199086c6b67f4b3560344a6d7335
Lashi Thendra <lashithendrabandara@gmail.com>
2023-08-18 02:49:47 +0530

    handle exception in customer controller

--- back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
@@ -7,9 +7,15 @@ import lk.ijse.dep10.pos.dto.CustomerDTO;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -18,6 +24,27 @@ public class CustomerController {
 
     @Autowired
     private BasicDataSource pool;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleValidationException(MethodArgumentNotValidException exp) {
+        LinkedHashMap<String, Object> errorAttributes = new LinkedHashMap<>();
+        errorAttributes.put("timestamp", LocalDateTime.now().toString());
+        errorAttributes.put("status", 400);
+        errorAttributes.put("error", HttpStatus.BAD_REQUEST);
+        errorAttributes.put("message", "Data validation failed");
+
+        List<Map<String, Object>> errorList = new ArrayList<>();
+        for (FieldError fieldError : exp.getFieldErrors()) {
+            Map<String, Object> error = new LinkedHashMap<>();
+            error.put("field", fieldError.getField());
+            error.put("rejected", fieldError.getRejectedValue());
+            error.put("message", fieldError.getDefaultMessage());
+            errorList.add(error);
+        }
+        errorAttributes.put("errors", errorList);
+        return errorAttributes;
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{id}")
