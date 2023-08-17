8b659c875236cd1fd2edd2c4ba9336bba2ac92ac
Lashi Thendra <lashithendrabandara@gmail.com>
2023-08-18 02:39:29 +0530

    add validations for CustomerDTO with hibernate validator

--- back-end/src/main/java/lk/ijse/dep10/pos/dto/CustomerDTO.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/dto/CustomerDTO.java
@@ -1,16 +1,30 @@
 package lk.ijse.dep10.pos.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDTO {
+
+
     private Integer id;
+
+    @NotBlank(message = "Name can't be empty or null")
+    @Pattern(regexp = "[A-Za-z]+", message = "Invalid name")
     private String name;
+
+    @NotBlank(message = "Address can't be empty or null")
+    @Length(min=3, message="Invalid address")
     private String address;
+
+    @NotBlank(message = "Contact number can't be empty or null")
+    @Pattern(regexp = "^\\d{3}-\\d{7}$", message = "Invalid contact number")
     private String contact;
 }
