8bbf65a65c5ad27c5d2bee63ae000773b99d17f9
Lashi Thendra <lashithendrabandara@gmail.com>
2023-08-18 11:36:52 +0530

    inject datasource attributes via environment

--- back-end/src/main/java/lk/ijse/dep10/pos/WebRootConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebRootConfig.java
@@ -4,19 +4,23 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class WebRootConfig {
 
     @Bean
-    public BasicDataSource dataSource(){
+    public BasicDataSource dataSource(Environment env){
         BasicDataSource bds = new BasicDataSource();
-        bds.setUsername("root");
-        bds.setPassword("mysql");
-        bds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        bds.setUrl("jdbc:mysql://localhost:3306/dep10_pos");
-        bds.setMaxTotal(50);
-        bds.setInitialSize(20);
-        return  bds;
+        bds.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        bds.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        bds.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        bds.setUrl(env.getRequiredProperty("spring.datasource.url"));
+        bds.setMaxTotal(env.getRequiredProperty("spring.datasource.dbcp2.max-total", Integer.class));
+        bds.setInitialSize(env.getRequiredProperty("spring.datasource.dbcp2.initial-size", Integer.class));
+        return bds;
     }
+
 }
